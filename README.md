8664a103f06c3612a642c2566f90c8c53d310ede
Jarris <info@darkjarris.com>
2018-09-07 19:59:23 +0200

    Added temperature, fan speed readout, added verbosity switch, added help menu. Updated readme & Revisions

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # amdgpu-pro-fans
 
 
-Original creator DominiLux 
-Project extended: Sysdrum
+Original creator DominiLux  
+Project extended: Sysdrum  
 License: Apache Version 2.0
 
 This was an alpha release written in unix style bash script. It is a simple utility that allows you to set the fan speeds for AMD cards that support use the AMD GPU PRO driver. 
@@ -18,7 +18,7 @@ The current master branch is the stable release.
 * If you move amdgpu-pro-fans just Run the ./create_desktop_file again to update everything.
 
 
-## Dependencies : 
+## Dependencies: 
 * dialog - is required to for the menu to work.
 
 ## Adjusting the MENU Example:
@@ -28,10 +28,62 @@ The current master branch is the stable release.
 * You can edit fanspeeddlg-promenu.sh and update the ranges. 
 * At anytime you can run ./create_desktop_file.sh to reset the changes to default.
 
+## CLI Usages:
+
+sudo ./amdgpu-pro-fans.sh -s [speed 0 - 100]  
+./amdgpu-pro-fans.sh -t  
+./amdgpu-pro-fans.sh -r  
+./amdgpu-pro-fans.sh -h  
+./amdgpu-pro-fans.sh -v [0,1] (default 1)
+
+### Examples
+sudo ./amdgpu-pro-fans.sh -s 80  
+This would set all of your fan speeds to 80 percent of their maximum speed.
+
+./amdgpu-pro-fans.sh -t  
+This will show the current temperature in Centigrade.
+
+./amdgpu-pro-fans.sh -r  
+This will show current fan speed percentage.
+
+./amdgpu-pro-fans.sh -h  
+This shows the help feature
+
+./amdgpu-pro-fans.sh -v  
+This sets verbosity. 0 is simple output, 1 is verbose output. default is 1. 
+
+
+If you wish to change verbosity to 0, use the flag before asking for output, examples given:  
+./amdgpu-pro-fans.sh -v 1 -r  
+Fan speed on Card0 is 100%
+
+./amdgpu-pro-fans.sh -v 0 -r  
+100
+
+./amdgpu-pro-fans.sh -v 1 -a 0 -r  
+Fan speed on Card0 is 100%
+
+./amdgpu-pro-fans.sh -v 0 -a 0 -r  
+100
+
+./amdgpu-pro-fans.sh -t -v 0  
+$0: invalid option -- ' '  
+$0: invalid option -- '-'  
+Temperature on Card0 is 51%
+
+./amdgpu-pro-fans.sh -t -v 0 -a 0  
+$0: invalid option -- ' '  
+$0: invalid option -- '-'  
+Temperature on Card0 is 51%
+
+By using these features, it is possible to make a GUI front-end that can run this script and interpret the answers. If writing a GUI, set verbosity to 0 on your calls to get simple answers back you can check against.
+
+
 ## Notes:
-Fully tested on Ubuntu 18.04 with AMDGPU-PRO proprietary linux drivers. It is compatible with all Radeon R8 Series, R9 Series, and RX Series graphics cards.
+Fully tested on Ubuntu 18.04 with AMDGPU-PRO proprietary Linux drivers. It is compatible with all Radeon R8 Series, R9 Series, and RX Series graphics cards.
+
 
 ## Orignal project:
-https://github.com/DominiLux/amdgpu-pro-fans
-Dev replaced the project with:
+https://github.com/DominiLux/amdgpu-pro-fans  
+Dev replaced the project with:  
 https://github.com/DominiLux/GPUMAGIC
--- amdgpu-pro-fans.sh
+++ amdgpu-pro-fans.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #####################################
-#  AMDGPU-PRO LINUX UTILITIES SUITE  #
+#AMDGPU-PRO LINUX UTILITIES SUITE#
 ######################################
 # Utility Name: AMDGPU-PRO-FANS
-# Version: 0.1.9
+# Version: 0.2.0
 # Version Name: Sysdrum
 # https://github.com/sysdrum/amdgpu-pro-fans
 
@@ -23,7 +23,7 @@
 
 
 #####################################################################
-#                          *** IMPORTANT ***                        #
+#								*** IMPORTANT ***									  #
 # DO NOT MODIFY PAST THIS POINT IF YOU DONT KNOW WHAT YOUR DOING!!! # 
 #####################################################################
 
@@ -34,15 +34,21 @@ adapter="all"
 targettemp=""
 fanpercent=""
 arguments="$@"
+verbosity="1"
 ##################
 # USAGE FUNCTION #
 ##################
 usage ()
 {
-    echo "* AMDGPU-PRO-FANS *"
-    echo "error: invalid arguments"
-    echo "usage: $0 [-h] for help..."
-    exit
+	echo "* AMDGPU-PRO-FANS *"
+	echo "error: invalid arguments"
+	echo "usage: $0 [-a <0,1,2,3...> to set card number]"
+	echo "usage: $0 [-s <0-100>] to set fan speed percentage"
+	echo "usage: $0 [-r] to read current fan speed percentage"
+	echo "usage: $0 [-t] to read current temperature"
+	echo "usage: $0 [-v <0-1>] to change verbosity. 0 = none. 1 = verbose."
+	echo "usage: $0 [-h] for help..."
+	exit
 }
 
 ###########################
@@ -51,70 +57,122 @@ usage ()
 
 set_all_fan_speeds ()
 {
-    cardcount="0";
-    for CurrentCard in  /sys/class/drm/card?/ ; do
-         for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ ; do
-              cd $CurrentMonitor # &>/dev/null
-              workingdir="`pwd`"
-              fanmax=$(head -1 "$workingdir"/pwm1_max)
-              if [ $fanmax -gt 0 ] ; then    
-                  speed=$(( fanmax * fanpercent ))
-                  speed=$(( speed / 100 ))
-                  sudo chown $USER "$workingdir"/pwm1_enable
-                  sudo chown $USER "$workingdir"/pwm1
-                  sudo echo -n "1" >> $workingdir/pwm1_enable # &>/dev/null
-                  sudo echo -n "$speed" >> $workingdir/pwm1 # &>/dev/null
-                  speedresults=$(head -1 "$workingdir"/pwm1)
-                  if [ $(( speedresults - speed )) -gt 6 ] ; then
-                       echo "Error Setting Speed For Card$cardcount!"
-                  else
-                       echo "Card$cardcount Speed Set To $fanpercent %"
-                  fi
-              else
-                  echo "Error: Unable To Determine Maximum Fan Speed For Card$cardcount!"
-              fi
-         done
-         cardcount="$(($cardcount + 1))"
-    done
+	cardcount="0";
+	for CurrentCard in /sys/class/drm/card?/ ; do
+		for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ ; do
+			cd $CurrentMonitor # &>/dev/null
+			workingdir="`pwd`"
+			fanmax=$(head -1 "$workingdir"/pwm1_max)
+			if [ $fanmax -gt 0 ] ; then	
+				speed=$(( fanmax * fanpercent ))
+				speed=$(( speed / 100 ))
+				sudo chown $USER "$workingdir"/pwm1_enable
+				sudo chown $USER "$workingdir"/pwm1
+				sudo echo -n "1" >> $workingdir/pwm1_enable # &>/dev/null
+				sudo echo -n "$speed" >> $workingdir/pwm1 # &>/dev/null
+				speedresults=$(head -1 "$workingdir"/pwm1)
+				if [ $(( speedresults - speed )) -gt 6 ] ; then
+					if [ "$verbosity" -eq 0 ] ; then echo "-2" ; fi
+					if [ "$verbosity" -eq 1 ] ; then echo "Error Setting Speed For Card$cardcount!" ; fi
+				else
+					if [ "$verbosity" -eq 0 ] ; then echo "$fanpercent" ; fi
+					if [ "$verbosity" -eq 1 ] ; then echo "Card$cardcount Speed Set To $fanpercent %" ; fi 
+				fi
+			else
+				if [ "$verbosity" -eq 0 ] ; then echo "-1" ; fi
+				if [ "$verbosity" -eq 1 ] ; then echo "Error: Unable To Determine Maximum Fan Speed For Card$cardcount!" ; fi
+			fi
+		done
+		cardcount="$(($cardcount + 1))"
+	done
 }
 
 set_single_gpu_fan_speeds ()
 {
-    CurrentCard="/sys/class/drm/card$adapter/"
-    if [ -d $CurrentCard ]; then
-	for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ ; do
-	cd $CurrentMonitor # &>/dev/null
-	workingdir="`pwd`"
-	fanmax=$(head -1 "$workingdir"/pwm1_max)
-	if [ $fanmax -gt 0 ] ; then    
-	    speed=$(( fanmax * fanpercent ))
-	    speed=$(( speed / 100 ))
-	    sudo chown $USER "$workingdir"/pwm1_enable
-	    sudo chown $USER "$workingdir"/pwm1
-	    sudo echo -n "1" >> $workingdir/pwm1_enable # &>/dev/null
-	    sudo echo -n "$speed" >> $workingdir/pwm1 # &>/dev/null
-	    speedresults=$(head -1 "$workingdir"/pwm1)
-	    if [ $(( speedresults - speed )) -gt 6 ] ; then
-		 echo "Error Setting Speed For Card$adapter!"
-	    else
-		 echo "Card$adapter Speed Set To $fanpercent %"
-	    fi
+	CurrentCard="/sys/class/drm/card$adapter/"
+	if [ -d $CurrentCard ]; then
+		for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ ; do
+			cd $CurrentMonitor # &>/dev/null
+			workingdir="`pwd`"
+			fanmax=$(head -1 "$workingdir"/pwm1_max)
+			if [ $fanmax -gt 0 ] ; then	
+				speed=$(( fanmax * fanpercent ))
+				speed=$(( speed / 100 ))
+				sudo chown $USER "$workingdir"/pwm1_enable
+				sudo chown $USER "$workingdir"/pwm1
+				sudo echo -n "1" >> $workingdir/pwm1_enable # &>/dev/null
+				sudo echo -n "$speed" >> $workingdir/pwm1 # &>/dev/null
+				speedresults=$(head -1 "$workingdir"/pwm1)
+				if [ $(( speedresults - speed )) -gt 6 ] ; then
+					if [ "$verbosity" -eq 0 ] ; then echo "-2" ; fi
+					if [ "$verbosity" -eq 1 ] ; then echo "Error Setting Speed For Card$adapter!" ; fi 
+				else
+					if [ "$verbosity" -eq 0 ] ; then echo "$fanpercent" ; fi
+					if [ "$verbosity" -eq 1 ] ; then echo "Card$adapter Speed Set To $fanpercent %" ; fi 
+				fi
+			else
+				if [ "$verbosity" -eq 0 ] ; then echo "-1" ; fi
+				if [ "$verbosity" -eq 1 ] ; then echo "Error: Unable To Determine Maximum Fan Speed For Card$adapter!" ; fi
+			fi
+		done
 	else
-	    echo "Error: Unable To Determine Maximum Fan Speed For Card$adapter!"
+		if [ "$verbosity" -eq 0 ] ; then echo "-3" ; fi
+		if [ "$verbosity" -eq 1 ] ; then echo "Error: Unable to find the specified card" ; fi
 	fi
-	done
-    else
-	echo "Error: Unable to find the specified card"
-    fi
 }
+
+read_all_fan_speeds ()
+{
+	cardcount="0";
+	for CurrentCard in /sys/class/drm/card?/ ; do
+		for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ ; do
+			cd $CurrentMonitor # &>/dev/null
+			workingdir="`pwd`"
+			fanspeed=$(head -1 "$workingdir"/pwm1)
+			fanmax=$(head -1 "$workingdir"/pwm1_max)
+			if [ $fanspeed -gt 0 ] ; then
+				speedf=$( echo "scale=2; $fanspeed / $fanmax * 100" | bc)
+				speed=${speedf%.*}
+				if [ "$verbosity" -eq 0 ] ; then echo "$speed" ; fi
+				if [ "$verbosity" -eq 1 ] ; then echo "Fan speed on Card$cardcount is $speed%" ; fi			
+			else
+				if [ "$verbosity" -eq 0 ] ; then echo "-1" ; fi
+				if [ "$verbosity" -eq 1 ] ; then echo "Error: Unable To Determine Fan Speed For Card$cardcount!" ; fi
+			fi
+		done
+		cardcount="$(($cardcount + 1))"
+	done
+}
+
+read_current_temperature ()
+{
+	cardcount="0";
+	for CurrentCard in /sys/class/drm/card?/ ; do
+		for CurrentMonitor in "$CurrentCard"device/hwmon/hwmon?/ ; do
+			cd $CurrentMonitor # &>/dev/null
+			workingdir="`pwd`"
+			temp=$(head -1 "$workingdir"/temp1_input)
+			temp=$(( $temp/1000 ))
+			if [ $temp -gt 0 ] ; then
+				if [ "$verbosity" -eq 0 ] ; then echo "$temp" ; fi
+				if [ "$verbosity" -eq 1 ] ; then echo "Temperature on Card$cardcount is $temp%" ; fi
+			else
+				if [ "$verbosity" -eq 0 ] ; then echo "-1" ; fi	
+				if [ "$verbosity" -eq 1 ] ; then echo "Error: Unable To Determine Card Temperature For Card$cardcount!" ; fi
+			fi
+		done
+		cardcount="$(($cardcount + 1))"
+	done
+}
+
 
 set_fans_requested ()
 {
-    if [ "$adapter" = "all" ]; then
-        set_all_fan_speeds
-    else
-	set_single_gpu_fan_speeds
-    fi
+	if [ "$adapter" = "all" ]; then
+		set_all_fan_speeds
+	else
+		set_single_gpu_fan_speeds
+	fi
 }
 
 
@@ -123,16 +181,20 @@ set_fans_requested ()
 #################################
 command_line_parser ()
 {
-     parseline=`getopt -s bash -u -o a:s: -n '$0' -- "$arguments"` 
-     eval set -- "$parseline"
-     while true ; do
-        case "$1" in
-            -a ) adapter="$2" ; shift 2 ;;
-            -s ) fanpercent="$2" ; set_fans_requested ; break ;;
-            --)  break ;;
-            *) usage ; exit 1 ;;
-        esac    
-    done
+	parseline=`getopt -s bash -u -o a:s:rhtv: -n '$0' -- "$arguments"`
+	eval set -- "$parseline"
+	while true ; do
+		case "$1" in
+				-a ) adapter="$2" ; shift 2 ;;
+				-v ) verbosity="$2" ; shift 2 ;;
+				-s ) fanpercent="$2" ; set_fans_requested ; shift 2 ; break;;
+				-r ) read_all_fan_speeds ; shift 2 ; break;;
+				-t ) read_current_temperature ; shift 2 ; break;;
+				--)break ;;
+				-h) usage ; exit 1 ;;
+				*) usage ; exit 1 ;;
+		esac
+	done
 }
 
 #################
